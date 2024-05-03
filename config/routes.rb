d269729e69f881cc5de74db0ac4895a1937d91a0
MoeMaeda <160582011+moe-maeda@users.noreply.github.com>
2024-05-03 13:54:08 +0000

    [Add]devise初期設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
