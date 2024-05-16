164c14762393ce80010b89745f75caf7b805eccf
daisy26 <160582011+moe-maeda@users.noreply.github.com>
2024-05-16 13:25:24 +0000

    [Update]search修正

--- test/controllers/public/searches_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Public::SearchesControllerTest < ActionDispatch::IntegrationTest
-  test "should get search" do
-    get public_searches_search_url
-    assert_response :success
-  end
-end
