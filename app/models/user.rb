b2a4c629c687bd6cb01928208b2558bd9a0fdcfa
daisy26 <160582011+moe-maeda@users.noreply.github.com>
2024-05-27 14:11:13 +0000

    [Remove]非同期ボタン不具合,デザイン修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
 
   def get_profile_image(width, height)
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      file_path = Rails.root.join('app/assets/images/top_logo.png')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
