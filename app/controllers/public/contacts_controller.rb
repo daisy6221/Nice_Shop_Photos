cb3ac0feab2fc492117fba49d61f5b74fa55385e
daisy26 <160582011+moe-maeda@users.noreply.github.com>
2024-07-27 14:46:49 +0000

    [Fix]mailerの本番環境修正

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -18,10 +18,10 @@ class Public::ContactsController < ApplicationController
         ContactMailer.done_mail(@contact).deliver_now
         ContactMailer.send_mail(@contact).deliver_now
         redirect_to done_contacts_path
-      rescue StandardError => e
-        puts "メールの送信に失敗しました。エラー：#{e.message}"
-        flash.now[:alert] = "メールの送信に失敗しました。メールアドレスを見直してください。"
-        render "new"
+      # rescue StandardError => e
+      #   puts "メールの送信に失敗しました。エラー：#{e.message}"
+      #   flash.now[:alert] = "メールの送信に失敗しました。メールアドレスを見直してください。"
+      #   render "new"
       end
     else
       render "new"
